3a5ba22efc2130ee8747690ec53ee2eba8b482f2
Amirhz35 <127244781+Amirhz35@users.noreply.github.com>
2023-03-10 23:28:41 +0330

    Add files via upload

---
+++ switch.cs
@@ -0,0 +1,131 @@
+﻿//amir hossein zarei
+
+
+using System.Diagnostics;
+
+string ans,ans1;
+
+
+Console.WriteLine("please notice that you can only use *Y/y* or *N/n*");
+do
+{
+    Console.WriteLine("are you married??");
+    lockkey();
+    switch (ans)
+    {
+        case "y":
+        case "Y":
+            Console.WriteLine("have you any kids??");
+            lockkey();
+            switch (ans)
+            {
+                case "y":
+                case "Y":
+                    Console.WriteLine("do you want more kids??");
+                    lockkey();
+                    switch (ans)
+                    {
+                        case "y":
+                        case "Y":
+                            Console.WriteLine("is your wife ok with that??");
+                            lockkey();
+                            switch (ans)
+                            {
+                                case "y":
+                                case "Y":
+                                    Console.WriteLine("I'll hope your kid will be safe.");
+                                    break;
+                                case "n":
+                                case "N":
+                                    Console.WriteLine("I'll hope you can convince your wife.");
+                                    break;
+                            }
+                            break;
+                        case "n":
+                        case "N":
+                            Console.WriteLine("I'll hope you have happy life.");
+                            break;
+                    }
+                    break;
+                case "n":
+                case "N":
+                    Console.WriteLine("don't you want a kid??");
+                    lockkey();
+                    switch (ans)
+                    {
+                        case "y":
+                        case "Y":
+                            Console.WriteLine("I'll hope someday you reach your wish.");
+                            break;
+                        case "n":
+                        case "N":
+                            Console.WriteLine("ohh that's a sad thing to hear kids are so sweat.");
+                            break;
+                    }
+                    break;
+            }
+            break;
+        case "n":
+        case "N":
+            Console.WriteLine("are you going to married??");
+            lockkey();
+            switch (ans)
+            {
+                case "y":
+                case "Y":
+                    Console.WriteLine("do you find right person??");
+                    lockkey();
+                    switch (ans)
+                    {
+                        case "y":
+                        case "Y":
+                            Console.WriteLine("I'll hope you marry her someday.");
+                            break;
+                        case "n":
+                        case "N":
+                            Console.WriteLine("I'll hope you find right person.");
+                            break;
+                    }
+                    break;
+                case "n":
+                case "N":
+                    Console.WriteLine("alright i wont disturb you any more.");
+                    break;
+            }
+            break;
+    }
+    lockkeyend();
+} while (ans1.ToLower() == "y");
+Console.WriteLine("thank you for using this program <3");
+
+
+
+
+
+
+
+void lockkeyend()
+{
+
+    Console.WriteLine("do you want to try again?? pls press *y* \nif you don't want to try again pls press *n*");
+    do
+    {
+        ans1 = Console.ReadKey(true).KeyChar.ToString();
+
+    } while (ans1.ToLower() != "y" && ans1.ToLower() != "n");
+}
+
+
+
+
+
+    void lockkey()
+{
+    do
+    {
+        ans = Console.ReadKey(true).KeyChar.ToString();
+
+    } while (ans.ToLower() != "y" && ans.ToLower() != "n");
+}
+
+
---
+++ tamrin5.cs
@@ -0,0 +1,83 @@
+﻿//amir hossein zarei
+
+int x;
+int y;
+ConsoleKeyInfo digit;
+string output;
+
+
+
+do
+{
+    Console.WriteLine("please enter your grade:");
+    x = 0;
+    do
+    {
+        lockkeynum();
+        Calculatex();
+
+    } while (digit.Key.ToString() != "Enter");
+
+    calculategrade();
+    lockkeyyn();
+} while (output.ToLower() == "y");
+Console.WriteLine("thank you for using this program <3");
+
+
+
+
+
+
+
+void calculategrade()
+{
+    if (20 < x || x < 0)
+        Console.WriteLine("oh i think you made a mistake, your grade should be between 0 and 20");
+    else
+    {
+        if (17 < x && x <= 20)
+            Console.WriteLine("your grade rank is A");
+        else if (15 < x && x <= 17)
+            Console.WriteLine("your grade rank is B");
+        else if (10 < x && x <= 15)
+            Console.WriteLine("your grade rank is C");
+        else
+            Console.WriteLine("oh sorry you are failed");
+        y = x * 5;
+        Console.WriteLine($"you have achieved {y}% of the total grade");
+    }
+}
+
+
+
+void lockkeynum()
+{
+    do
+    {
+        digit = Console.ReadKey(true);
+
+    } while(digit.KeyChar.ToString() != "0" && digit.KeyChar.ToString() != "1" &&
+            digit.KeyChar.ToString() != "2" && digit.KeyChar.ToString() != "3" &&
+            digit.KeyChar.ToString() != "4" && digit.KeyChar.ToString() != "5" &&
+            digit.KeyChar.ToString() != "6" && digit.KeyChar.ToString() != "7" &&
+            digit.KeyChar.ToString() != "8" && digit.KeyChar.ToString() != "9" &&
+            digit.Key.ToString() != "Enter");
+}
+void Calculatex()
+{
+    if (digit.Key.ToString() != "Enter")
+        x = x * 10 + Convert.ToInt32(digit.KeyChar.ToString());
+        Console.Write(digit.KeyChar);
+}
+
+
+void lockkeyyn()
+{
+    Console.WriteLine("do you want to continue?? pls press *y* \n" +
+        "if you don't want to continue pls press *n*");
+    do
+    {
+        output = Console.ReadKey(true).KeyChar.ToString();
+
+    } while (output.ToLower() != "y" && output.ToLower() != "n");
+}
